b8abff6cc92d06a60df7a663fac4e1f582785808
qilin70007 <70007qilin@gmail.com>
2026-07-12 15:56:13 +0800

    slow and naturalize example speech

--- public/sw.js
+++ public/sw.js
@@ -1,13 +1,13 @@
 'use strict';
 
-const CACHE_NAME = 'vocab-master-v2.3.1';
+const CACHE_NAME = 'vocab-master-v2.3.2';
 const SHELL = [
   '/',
   '/index.html',
   '/mobile.html',
   '/words.json',
-  '/style.css?v=2.3.1',
-  '/app.js?v=2.3.1',
+  '/style.css?v=2.3.2',
+  '/app.js?v=2.3.2',
   '/manifest.webmanifest',
   '/icons/icon.svg'
 ];
